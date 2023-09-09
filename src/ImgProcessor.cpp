d542fdf3a00b920b3915beb41f6cdab19a47c42b
jjxia81 <xiajianjun2011@hotmail.com>
2023-09-09 14:10:29 -0500

    back up coda and data

--- src/ImgProcessor.cpp
+++ src/ImgProcessor.cpp
@@ -1,15 +1,16 @@
 #include "ImgProcessor.hpp"
 #include <fstream>
 #include <iostream>
-
+#include <string>
+#include "opencv2/imgproc.hpp"
 
 void ShowImg(cv::Mat img, const std::string& imgName)
 {
-    double min,max;
-    cv::minMaxLoc(img,&min,&max);
-    auto new_img = img / max;
+    // double min,max;
+    // cv::minMaxLoc(img,&min,&max);
+    // auto new_img = img / max;
     // std::cout << "min max : " << min << " " << max << std::endl;
-    cv::imshow(imgName, new_img);
+    cv::imshow(imgName, img);
     cv::waitKey(0);
 }
 
@@ -17,16 +18,6 @@ void ImgProcessor::LoadImgStacks(const std::string& img_stack_path)
 {
     this->img_stacks_.clear();
 	cv::imreadmulti(img_stack_path, this->img_stacks_, cv::IMREAD_ANYDEPTH);
-	// for (const auto& img : this->img_stacks_)
-	// {
-	// 	double min,max;
-	// 	cv::minMaxLoc(img,&min,&max);
-	// 	auto new_img = img / max;
-	// 	// std::cout << "min max : " << min << " " << max << std::endl;
-	// 	cv::imshow("first img", new_img);
-	// 	cv::waitKey(0);
-	// 	break;
-	// }
     volume_z_max_ = img_stacks_.size();
     std::cout << "img stack num count : " << volume_z_max_ << std::endl;
     if(volume_z_max_ > 0)
@@ -35,6 +26,282 @@ void ImgProcessor::LoadImgStacks(const std::string& img_stack_path)
         volume_y_max_ = img_stacks_[0].rows;
         std::cout << "img cols : " << volume_x_max_ << std::endl;
         std::cout << "img rows : " << volume_y_max_ << std::endl;
+    }
+}
+
+
+void ImgProcessor::LoadImgStacksMask(const std::string& img_stack_mask_path)
+{
+    this->img_mask_stacks_.clear();
+    // std::cout << "mask path : " << img_stack_mask_path <<
+	cv::imreadmulti(img_stack_mask_path, this->img_mask_stacks_, cv::IMREAD_ANYDEPTH);
+    if(this->img_mask_stacks_.empty())
+    {
+        std::cout << " Load image mask stack failed !" << std::endl;
+    } else {
+        std::cout << " Load image mask stack succeed !" << std::endl;
+        std::cout << " Image mask size : "<< this->img_mask_stacks_.size() << std::endl;
+    }
+}
+
+void ImgProcessor::LoadImgContourSamplePoints(const std::string& img_contour_sample_points_path)
+{
+    std::string line;
+    std::ifstream myfile(img_contour_sample_points_path);
+    if (myfile.is_open())
+    {
+        std::cout << " Load Contour Sample Points succeed !" << std::endl;
+        this->contour_sample_points_.clear();
+        while ( getline (myfile,line) )
+        {
+            // std::cout << line << '\n';
+            std::string word = "";
+            std::vector<int> coords;
+            for (auto s_i : line)
+            {
+                if (s_i == ' ')
+                {
+                   
+                    coords.push_back(std::stoi(word));
+                    word = "";
+                }
+                else {
+                    word = word + s_i;
+                }
+            }
+            coords.push_back(std::stoi(word));
+
+            // std::cout << "coords size : " << coords.size() << std::endl;
+            if(coords.size() == 3)
+            {
+                Point3i new_p(coords[0], coords[1], coords[2]);
+                this->contour_sample_points_.push_back(new_p);
+            }
+        
+        }
+        myfile.close();
+        std::cout << " Load Contour Sample Points number: "<<  this->contour_sample_points_.size() << std::endl;
+    }
+}
+
+Mat ImgProcessor::CalStructureTensor(const Vec3& gradeint)
+{
+    Mat tensor(3,3, CV_32F);
+    Mat grad(gradeint);
+
+    Mat tensor =  grad * grad.t();
+    return tensor;
+}
+
+
+Mat CalSamplePointsStructureTensor()
+{
+    
+
+}
+
+
+Vec3 ImgProcessor::CalPointGradient(const Point3i& point)
+{
+        const auto& img_p = point; 
+        // std::cout << img_p.x << " " << img_p.y << " " << img_p.z<< std::endl;
+        double d_x = 0;
+        double d_y = 0;
+        double d_z = 0;
+        if(img_p.z == 0)
+        {
+            double z_cur  = this->img_stacks_[img_p.z].at<double>(img_p.y, img_p.x);
+            double z_back = this->img_stacks_[img_p.z + 1].at<double>(img_p.y, img_p.x);
+            d_z =  z_back - z_cur;
+        } else if (img_p.z == this->img_stacks_.size() - 1){
+            double z_cur  = this->img_stacks_[img_p.z].at<double>(img_p.y, img_p.x);
+            double z_pre = this->img_stacks_[img_p.z - 1].at<double>(img_p.y, img_p.x);
+            d_z = z_cur - z_pre;
+        } else {
+           
+            double z_pre = this->img_stacks_[img_p.z - 1].at<double>(img_p.y, img_p.x);
+            double z_back = this->img_stacks_[img_p.z + 1].at<double>(img_p.y, img_p.x);
+            d_z = (z_back - z_pre)/ 2.0;
+        }
+        const auto& cur_img = this->img_stacks_[img_p.z];
+        if(img_p.x == 0)
+        {
+            double x_cur  =cur_img.at<double>(img_p.y, img_p.x);
+            double x_right = cur_img.at<double>(img_p.y, img_p.x + 1);
+            d_x =  x_right - x_cur;
+        } else if (img_p.x == cur_img.cols - 1){
+            double x_cur  = cur_img.at<double>(img_p.y, img_p.x);
+            double x_left  = cur_img.at<double>(img_p.y, img_p.x - 1);
+            d_x = x_left - x_cur;
+        } else {
+            double x_left = cur_img.at<double>(img_p.y, img_p.x - 1 );
+            double x_right = cur_img.at<double>(img_p.y, img_p.x + 1);
+            d_x = (x_right - x_left)/ 2.0;
+        }
+
+        if(img_p.y == 0)
+        {
+            double y_cur  =cur_img.at<double>(img_p.y, img_p.x);
+            double y_top = cur_img.at<double>(img_p.y + 1, img_p.x);
+            d_y =  y_top - y_cur;
+        } else if (img_p.y == cur_img.rows - 1){
+            double y_cur  =cur_img.at<double>(img_p.y, img_p.x);
+            double y_bot = cur_img.at<double>(img_p.y - 1, img_p.x);
+            d_y =  y_cur - y_bot;
+        } else {
+            double y_bot = cur_img.at<double>(img_p.y - 1, img_p.x);
+            double y_top = cur_img.at<double>(img_p.y + 1, img_p.x);
+            d_y = (y_top - y_bot)/ 2.0;
+        }
+     
+        Vec3 gradient(d_x, d_y, d_z);
+        return gradient;
+}
+
+void ImgProcessor::CalSamplePointsGradients()
+{
+    std::cout << " img stack size : " << img_stacks_.size() << std::endl;
+    std::cout << " img stack  data type  : "  << img_stacks_[0].type();
+    for(size_t i = 0; i < this->contour_sample_points_.size(); ++i)
+    {
+        const auto& img_p = contour_sample_points_[i]; 
+        // std::cout << img_p.x << " " << img_p.y << " " << img_p.z<< std::endl;
+        Vec3 gradient = CalPointGradient(img_p);
+        this->contour_sample_points_gradients_.push_back(gradient);
+    }
+    std::cout << " Cal Contour Sample Points  gradient succeed, with gradient number: "<<  this->contour_sample_points_gradients_.size() << std::endl;
+
+}
+
+void ImgProcessor::SaveImgContourSamplePointsWithGradients(const std::string& img_contour_sample_points_path)
+{
+    std::fstream s_file{ img_contour_sample_points_path, s_file.out };
+    const auto& c_points = this->contour_sample_points_;
+    const auto& c_gradients = this->contour_sample_points_gradients_; 
+    for (size_t i = 0; i < c_points.size(); ++i)
+    {
+        auto point = c_points[i];
+        // auto gradient = volume_gradients_[i] ;
+        auto gradient = (c_gradients[i]);
+        // if(cv::norm(volume_gradients_[i]) > 0)
+        // {
+        //     std::cout << "gradient value : " << gradient << std::endl;
+        // }
+        gradient = cv::normalize(gradient) * cv::norm(gradient) / 1000  ;
+        auto px = point.x + gradient[0];
+        auto py = point.y + gradient[1];
+        auto pz = point.z + gradient[2];
+        auto new_p = Point3(px, py, pz);
+
+        std::string p_str = std::to_string(point.x) + " " + std::to_string(point.y) + " " + std::to_string(point.z);
+        s_file << "v " << p_str << std::endl;
+
+        std::string e_str = std::to_string(new_p.x) + " " + std::to_string(new_p.y) + " " + std::to_string(new_p.z);
+        s_file << "v " << e_str << std::endl;
+    }
+
+    for (size_t i = 0; i < c_points.size(); ++i)
+    {
+        std::string new_l = "l " + std::to_string(i *2 + 1) + " " + std::to_string(i *2 + 2);
+        s_file << new_l << std::endl;
+    }
+}
+
+
+
+
+cv::RNG rng(12345);
+
+void ImgProcessor::ScanImageEdgeDetection()
+{
+    size_t img_id = 0; 
+    for(size_t img_id = 0; img_id < this->img_stacks_.size(); ++ img_id)
+    {
+        if(img_id != 52) continue;
+        const auto& img = this->img_stacks_[img_id];
+        double minVal; 
+        double maxVal; 
+        cv::Point minLoc; 
+        cv::Point maxLoc;
+
+        minMaxLoc(img, &minVal, &maxVal);
+        
+
+        cv::Mat new_img = img; //(img - minVal) / (maxVal - minVal) * 255;
+        int lowThreshold = 0;
+        const int max_lowThreshold = 150;
+        const int ratio = 3;
+        const int kernel_size = 3;
+        const char* window_name = "Edge Map";
+
+        // new_img.convertTo(new_img, CV_8U);
+        auto img_type = new_img.type();
+
+
+        cv::Mat data;
+        new_img.convertTo(data,CV_32F);
+        data = data.reshape(1,data.total());
+
+        int K = 3;
+        // Term criteria for K-means algorithm
+        cv::TermCriteria termCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 100, 0.2);
+        cv::Mat labels, centers;
+        cv::kmeans(data, K, labels, termCriteria, 3, cv::KMEANS_RANDOM_CENTERS, centers);
+        // Reshape the labels back to the original image size
+        labels = labels.reshape(0, new_img.rows);
+        cv::Mat colormap(K, 1, CV_8U);
+        cv::RNG rng(12345);
+        for (int i = 0; i < K; ++i) {
+            colormap.at<uchar>(i, 0) = i * (256 / K);
+        }
+        cv::Mat segmentedImage(new_img.size(), CV_8U);
+        for (int i = 0; i < new_img.rows; ++i) {
+            for (int j = 0; j < new_img.cols; ++j) {
+                int clusterLabel = labels.at<int>(i, j);
+                segmentedImage.at<uchar>(i, j) = colormap.at<uchar>(clusterLabel, 0);
+            }
+        }
+
+        ShowImg(segmentedImage, "new segmentedImage");
+
+        segmentedImage.convertTo(new_img, CV_8U);
+        ShowImg(new_img, "new new_img");
+
+        std::cout << " first img id  " << std::endl;
+        // auto front_delt = cv::Mat::zeros(img_rows, img_cols, img_type);
+        cv::Mat detected_edges = cv::Mat::zeros(img.size(), img_type);
+        cv::Canny( new_img, detected_edges, max_lowThreshold, max_lowThreshold*ratio, kernel_size );
+
+        const auto& img_mask = this->img_mask_stacks_[img_id];
+        // ShowImg(img_mask, "new mask");
+        cv::Mat new_mask =  img_mask * 255;
+        new_mask.convertTo(new_mask, CV_8U);
+        std::vector<std::vector<cv::Point> > contours;
+        std::vector<cv::Vec4i> hierarchy;
+        // ShowImg(new_mask, "new mask");
+        findContours( new_mask, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE );
+        std::cout << " find contour succeed " << std::endl;
+
+        // detected_edges.convertTo(detected_edges, cv::CV_8UC3);
+        // cvtColor(detected_edges, detected_edges, cv::CV_g);
+        cv::Mat new_edges = cv::Mat::zeros(img.size(), img_type);
+        cv::cvtColor(detected_edges, new_edges, cv::COLOR_GRAY2BGR);
+        for( size_t i = 0; i< contours.size(); i++ )
+        {
+            cv::Scalar color = cv::Scalar(255, 122, 242,  0.5);
+            drawContours(new_edges, contours, (int)i, color, 1, cv::LINE_8, hierarchy, 0 );
+        }
+
+        // dst = Scalar::all(0);
+        std::cout << " visual img " << std::endl;
+        // src.copyTo( dst, detected_edges);
+        ShowImg(detected_edges, "new edges");
+        ShowImg(new_edges, "contours");
+        break;
+
+        // std::string out_img_path = this->contour_mask_overlay_output_path_ + "contour_mask_" + std::to_string(img_id) +".png";
+        // cv::imwrite(out_img_path,new_edges);
+
     }
 }
 
@@ -79,9 +346,6 @@ void ImgProcessor::CalVolumeGradients()
         cv::hconcat(padding_col, x_gradient_block, gradient_x);
         cv::hconcat(gradient_x, padding_col, gradient_x);
         
-
-        
-
         volume_gradient_x_.push_back(gradient_x);
         // std::cout << "gradient x success !" << std::endl;
 
@@ -103,18 +367,8 @@ void ImgProcessor::CalVolumeGradients()
         // auto bot_delt = bot_img - img_stacks_[i];
         // auto gradient_y = (up_delt + bot_delt) / 2.0;
         volume_gradient_y_.push_back(gradient_y); 
-
-        // if(i == 0)
-        // {
-        //     ShowImg(gradient_x, "gradient_x");
-        //     ShowImg(gradient_y, "gradient_y");
-
-        // }
-        // std::cout << "gradient y success !" << std::endl;
     }
-
     // ShowImg(volume_gradient_z_[0], "volume_gradient_z_");
-       
 }
 
 
@@ -206,15 +460,23 @@ void ImgProcessor::SaveGradients(const std::string& path)
     }
 }
 
-void ImgProcessor::RunGradientsPipeline(const std::string& img_stack_path, const std::string& out_path)
+void ImgProcessor::RunGradientsPipeline(const std::string& img_stack_path, const std::string& img_mask_stack_path, const std::string& out_path)
 {
     LoadImgStacks(img_stack_path);
+    // LoadImgStacksMask(img_mask_stack_path);
     std::cout << " Load Img stacks success !" << std::endl;
-    CalVolumeGradients();
-    std::cout << " CalVolumeGradients success !" << std::endl;
-    SampleVolumeGradients(sample_step_);
-     std::cout << " SampleVolumeGradients success !" << std::endl;
-    SaveGradients(out_path);
+    this->contour_mask_overlay_output_path_ = "../data/contour_mask_overlay/"; 
+    // ScanImageEdgeDetection();
+    std::string point_path = "/home/jjxia/Documents/projects/SparseRecon/IMG/6007/contour_points/opt_cv_contour_5/opt_cv_contour_4.xyz";
+    LoadImgContourSamplePoints(point_path);
+    CalSamplePointsGradients();
+    std::string gradient_path = "../data/gradients.obj";
+    SaveImgContourSamplePointsWithGradients(gradient_path);
+//     CalVolumeGradients();
+//     std::cout << " CalVolumeGradients success !" << std::endl;
+//     SampleVolumeGradients(sample_step_);
+//      std::cout << " SampleVolumeGradients success !" << std::endl;
+//     SaveGradients(out_path);
 }
 
 
--- src/ImgProcessor.hpp
+++ src/ImgProcessor.hpp
@@ -4,6 +4,8 @@
 
 typedef cv::Point3f Point3;
 typedef cv::Vec3f   Vec3;
+typedef cv::Point3i Point3i;
+typedef cv::Mat     Mat;
 
 class ImgProcessor{
 
@@ -12,12 +14,24 @@ class ImgProcessor{
         ~ImgProcessor(){};
         
         void LoadImgStacks(const std::string& img_stack_path);
+        void LoadImgStacksMask(const std::string& img_stack_mask_path);
+        void LoadImgContourSamplePoints(const std::string& img_contour_sample_points_path);
+        void SaveImgContourSamplePointsWithGradients(const std::string& img_contour_sample_points_path);
+        void CalSamplePointsGradients();
+        void CalSamplePointsStructureTensor();
+
+        void ScanImageEdgeDetection();
+
         void CalVolumeGradients();
         void SampleVolumeGradients(size_t sample_step);
         Vec3 GetGradient(size_t x_id, size_t y_id, size_t z_id);
         Vec3 GetGradient(const Point3& point);
         void SaveGradients(const std::string& path);
-        void RunGradientsPipeline(const std::string& img_stack_path, const std::string& out_path);
+        void RunGradientsPipeline(const std::string& img_stack_path, const std::string& img_mask_stack_path, const std::string& out_path);
+
+    private:
+        Mat CalStructureTensor(const Vec3& gradeint);
+        Vec3 CalPointGradient(const Point3i& point);
 
     public:
         size_t volume_x_max_;
@@ -27,11 +41,18 @@ class ImgProcessor{
     private:
 
         std::vector<cv::Mat> img_stacks_;
+        std::vector<cv::Mat> img_stacks_grey_;
+        std::vector<cv::Mat> img_mask_stacks_;
         std::vector<cv::Mat> volume_gradient_z_;
         std::vector<cv::Mat> volume_gradient_x_;
         std::vector<cv::Mat> volume_gradient_y_;
         std::vector<Point3>  volume_points_;
         std::vector<Vec3>    volume_gradients_;
+        std::vector<Point3i> contour_sample_points_;
+        std::vector<Vec3> contour_sample_points_gradients_;
+
+        std::string contour_mask_overlay_output_path_;
+
         float max_gradient_norm_;
         float sample_step_ = 4.0;
 };
